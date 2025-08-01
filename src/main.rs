1e05f297df1a93bcd6832eea6c98d7fd1ac19cbb
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-01 16:00:41 +0100

    Add some code to find LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::{
+    fs,
+    io::ErrorKind,
+    path::{Path, PathBuf},
+};
+
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
@@ -6,6 +12,7 @@ use ratatui::{
     text::Line,
     widgets::{Block, Paragraph},
 };
+use thiserror::Error;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -15,17 +22,96 @@ fn main() -> color_eyre::Result<()> {
     result
 }
 
+const SYS_CLASS_LEDS: &str = "/sys/class/leds";
+#[derive(Debug)]
+struct LED {
+    file_name: String,
+    name: String,
+    is_on: bool,
+}
+
+#[derive(Debug, Error)]
+enum NewLEDError {
+    #[error("LED does not exist")]
+    NotFound,
+    #[error("Invalid brightness value")]
+    InvalidBrightness,
+    /// File name is invalid UTF-8
+    #[error("Invalid encoding in file name")]
+    InvalidFileName,
+    #[error("I/O error: {0}")]
+    IOError(std::io::Error),
+}
+
+impl From<std::io::Error> for NewLEDError {
+    fn from(err: std::io::Error) -> Self {
+        Self::IOError(err)
+    }
+}
+
+impl LED {
+    pub fn new(file_name: String) -> Result<Self, NewLEDError> {
+        let led_path = PathBuf::from(SYS_CLASS_LEDS).join(&file_name);
+        fs::read_dir(&led_path).map_err(|e| match e.kind() {
+            ErrorKind::NotFound => NewLEDError::NotFound,
+            _ => NewLEDError::IOError(e),
+        })?;
+        let brightness_data = fs::read_to_string(&led_path.join("brightness"))?;
+        let brightness = brightness_data
+            .trim()
+            .parse::<u8>()
+            .map_err(|_| NewLEDError::InvalidBrightness)?;
+        Ok(Self {
+            name: file_name.clone().replace("::", " "),
+            file_name,
+            is_on: brightness > 0,
+        })
+    }
+}
+
+fn get_all_leds() -> Result<Vec<LED>, NewLEDError> {
+    let mut leds = Vec::new();
+    let directories = fs::read_dir(SYS_CLASS_LEDS).map_err(NewLEDError::IOError)?;
+    for directory in directories {
+        let directory = directory.map_err(NewLEDError::IOError)?;
+        let file_name = directory.file_name();
+        leds.push(LED::new(
+            file_name
+                .into_string()
+                .map_err(|_| NewLEDError::InvalidFileName)?,
+        )?);
+    }
+    Ok(leds)
+}
+
 /// The main application which holds the state and logic of the application.
 #[derive(Debug, Default)]
 pub struct App {
     /// Is the application running?
     running: bool,
+    leds: Vec<LED>,
+    log: Vec<String>,
 }
 
 impl App {
     /// Construct a new instance of [`App`].
     pub fn new() -> Self {
-        Self::default()
+        let mut log = Vec::new();
+        let leds = match get_all_leds() {
+            Ok(leds) => {
+                log.push(format!("Successfully found {} LED(s)", leds.len()));
+                leds
+            }
+            Err(e) => {
+                log.push(format!("Error getting LEDs: {}", e));
+                Vec::new()
+            }
+        };
+        Self {
+            running: false,
+            leds,
+            log,
+        }
     }
 
     /// Run the application's main loop.
@@ -45,19 +131,14 @@ impl App {
     /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
     /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
     fn render(&mut self, frame: &mut Frame) {
-        let title = Line::from("Ratatui Simple Template")
-            .bold()
-            .blue()
-            .centered();
-        let text = "Hello, Ratatui!\n\n\
-            Created using https://github.com/ratatui/templates\n\
-            Press `Esc`, `Ctrl-C` or `q` to stop running.";
+        let title = Line::from("Glimpse").bold().blue().centered();
+        let text = self.log.join("\n");
         frame.render_widget(
             Paragraph::new(text)
                 .block(Block::bordered().title(title))
-                .centered(),
+                .left_aligned(),
             frame.area(),
-        )
+        );
     }
 
     /// Reads the crossterm events and updates the state of [`App`].
